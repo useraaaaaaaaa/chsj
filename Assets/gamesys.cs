56806b35e8b4ba6d94950d79fff4bd390342ee8e
useraaaaaaaaa <1005281411@qq.com>
2024-05-28 14:24:41 +0800

    first commit

---
+++ Assets/gamesys.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Events;
+
+public class gamesys : MonoBehaviour
+{
+    public UnityEvent VisibleEvent, InvisibleEvent;
+    private void OnBecameVisible()
+    {
+        VisibleEvent?.Invoke();
+    }
+    private void OnBecameInvisible()
+    {
+        InvisibleEvent?.Invoke();
+    }
+
+    public void ChangeScene(string sceneName) 
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void GameQuit() 
+    {
+        Application.Quit();
+    }
+   
+}
